32ccd169350d6abc0360ed31ea3420cc2fc4aa51
Psuedoo <forthewin720@gmail.com>
2024-09-16 00:34:42 -0400

    complete day 2

--- 2023/day-2/main.py
+++ 2023/day-2/main.py
@@ -1,49 +1,103 @@
-from dataclasses import dataclass, field
+POSSIBLE_COUNTS = {"red": 12, "green": 13, "blue": 14}
+POSSIBLE_COLORS = ["red", "green", "blue"]
 
 
-@dataclass
-class Game:
-    number: int
-    cube_sets: list = field(default_factory=list)
-
-
-@dataclass
-class CubeSet:
-    game: Game
-    cubes: list = field(default_factory=list)
-
-
-@dataclass
 class Cube:
-    count: int
-    color: str
+    def __init__(self, color: str, count: int):
+        self.color = color
+        self.count = count
+
+    def is_possible(self):
+        return self.count <= POSSIBLE_COUNTS[self.color]
 
 
-def parse_line(line):
-    new_line = line.strip("Game :")
-    line_split = new_line.split(":")
-    game_number = line_split[0]
-    cube_sets = line_split[1].split(";")
-    game = Game(number=game_number)
-    parse_cube_sets(cube_sets, game)
+class Grab:
+    def __init__(self) -> None:
+        self.cubes = []
+        self.total_red = 0
+        self.total_green = 0
+        self.total_blue = 0
+
+    def add_cubes(self, cubes: list[str]):
+        for cube in cubes:
+            count, color = cube.strip().split(" ")
+            self.cubes.append(Cube(color=color, count=int(count)))
+
+    def is_possible(self):
+        return False not in [cube.is_possible() for cube in self.cubes]
+
+    def set_total_color_counts(self):
+        for cube in self.cubes:
+            match cube.color:
+                case "red":
+                    self.total_red += cube.count
+                case "green":
+                    self.total_green += cube.count
+                case "blue":
+                    self.total_blue += cube.count
 
 
-def parse_cube_sets(cube_sets, game):
-    for cube_set in cube_sets:
-        cubes = cube_set.split(",")
-        parse_cubes(cubes)
+class Game:
+    def __init__(self, label: str):
+        self.label = label
+        self.grabs = []
+        self.min_red = 0
+        self.min_green = 0
+        self.min_blue = 0
+        self.power_of_cubes = 0
 
+    @property
+    def number(self):
+        str_number = self.label.split(" ")[1].strip()
+        return int(str_number)
 
-def parse_cubes(cube_set):
-    for cube in cube_set:
-        clean_cube = cube.strip()
-        parsed_cube = clean_cube.split()
-        obj_cube = Cube(count=int(parsed_cube[0]), color=parsed_cube[1])
+    def add_grab(self, grab: Grab):
+        self.grabs.append(grab)
+
+    def is_possible(self):
+        return False not in [grab.is_possible() for grab in self.grabs]
+
+    def set_min_possible_colors(self):
+        for grab in self.grabs:
+            grab.set_total_color_counts()
+
+            if grab.total_red > self.min_red or self.min_red == 0:
+                self.min_red = grab.total_red
+
+            if grab.total_green > self.min_green or self.min_green == 0:
+                self.min_green = grab.total_green
+
+            if grab.total_blue > self.min_blue or self.min_blue == 0:
+                self.min_blue = grab.total_blue
+
+    def set_power_of_cubes(self):
+        self.power_of_cubes = self.min_red * self.min_green * self.min_blue
 
 
 with open("data.txt", "r") as f:
     data = f.readlines()
     games = []
+    total_possible_count = 0
+    total_powers = 0
+
     for line in data:
-        games.append(parse_line(line))
-        print(games)
+        clean_line = line.strip()
+        split = clean_line.split(":")
+        game_label = split[0].strip()
+        curr_game = Game(label=game_label)
+        games.append(curr_game)
+
+        grabs = split[1].split(";")
+        # Cleaning grab
+        for grab in grabs:
+            cubes = grab.split(",")
+            curr_grab = Grab()
+            curr_grab.add_cubes(cubes=cubes)
+            curr_game.add_grab(curr_grab)
+
+    for game in games:
+        game.set_min_possible_colors()
+        game.set_power_of_cubes()
+        total_powers += game.power_of_cubes
+
+    print(total_powers)
